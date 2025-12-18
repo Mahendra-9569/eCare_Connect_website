265fa37029dbfda9da701853f20ebfc5b8372122
Mahendra yadav <my7186695@gmail.com>
2025-12-18 17:21:11 +0530

    Update backend URL in Login component

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL
+  const backendUrl = https://ecare-connect-website-1.onrender.com
 
   const { setDToken } = useContext(DoctorContext)
   const { setAToken } = useContext(AdminContext)
